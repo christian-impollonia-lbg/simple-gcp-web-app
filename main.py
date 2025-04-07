2c16b24cb62ea3c92617f8059d20697fd7f8964d
christian-impollonia-lbg <christian.impollonia@lloydsbanking.com>
2025-04-07 17:26:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
   return 'Flask app is running!'
 
-port = int(os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=port)
