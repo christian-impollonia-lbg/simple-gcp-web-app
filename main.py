9a40cf1969cd394c5826dd55d6b36c9fd204abd0
christian-impollonia-lbg <christian.impollonia@lloydsbanking.com>
2025-04-07 17:24:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return 'Flask app is running!'
+
+port = int(os.environ.get("PORT", 8080))
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=port)
